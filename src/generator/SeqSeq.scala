a3ec58884ecb13d0bd8a57ad101d8ad01e139ef1
Stephen Twigg <sdtwigg@eecs.berkeley.edu>
2012-03-20 19:25:09 -0700

    bugfix so that x0_shadow is now selected properly

--- src/generator/SeqSeq.scala
+++ src/generator/SeqSeq.scala
@@ -5,8 +5,8 @@ import Rand._
 
 class SeqSeq(xregs: HWRegPool, fregs_s: HWRegPool, fregs_d: HWRegPool, mem: Mem) extends InstSeq
 {
-  val sub_seq = new SeqMem(xregs, mem)
-  if(!sub_seq.allocate_regs()) println("error: could not allocate ALU sub-sequence")
+  val sub_seq = new SeqALU(xregs)
+  if(!sub_seq.allocate_regs()) println("error: could not allocate sub-sequence")
   
   while(!sub_seq.is_done)
   {
