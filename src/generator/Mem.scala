295721998b94b9e9fb7ed088aacdd586f04798f5
Stephen Twigg <sdtwigg@eecs.berkeley.edu>
2012-03-21 22:02:22 -0700

    Bugfix to MemDump so it now works for memsize any multiple of 4 (and asserts failure otherwise), this should also fix vector init/output not always properly tracking VL

--- src/generator/Mem.scala
+++ src/generator/Mem.scala
@@ -3,19 +3,31 @@ package torture
 import scala.collection.mutable.ArrayBuffer
 import Rand._
 
-class Mem(name: Array[Operand], newsize: Int) extends Operand
+class Mem(name: Array[Operand], val  size: Int) extends Operand
 {
-  def this(namelabel: String, newsize: Int) = this(Array[Operand](Label(namelabel)), newsize)
+  def this(namelabel: String, size: Int) = this(Array[Operand](Label(namelabel)), size)
 
-  val size = newsize
+  assert(size % 4 == 0, "Memory size must be multiple of 4")
 
   override def toString = name.mkString("")
 
   def dumpdata = 
   {
     var s = this.toString + ":\n"
-    for (i <- 0 to (size/8/2 - 1))
-      s += "\t.dword 0x%016x, 0x%016x\n" format (rand_dword, rand_dword)
+    if(size % 16 == 0)
+    {
+      for (i <- 0 to (size/8/2 - 1))
+        s += "\t.dword 0x%016x, 0x%016x\n" format (rand_dword, rand_dword)
+    } else if(size % 8 == 0)
+    {
+      for (i <- 0 to (size/8 - 1))
+        s += "\t.dword 0x%016x\n" format (rand_dword)
+    }
+    else
+    {
+      for (i <- 0 to (size/4 - 1))
+        s += "\t.word 0x%08x\n" format (rand_word)
+    }
     s
   }
 }
